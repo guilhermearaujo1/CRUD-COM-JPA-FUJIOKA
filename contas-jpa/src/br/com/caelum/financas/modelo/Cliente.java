112b5fe0cb7cdce4fca2f763be4a0d479758dfd5
Guilherme Araujo <guilhermearaujopb@hotmail.com>
2018-08-27 23:39:02 -0300

    Add files via upload

---
+++ contas-jpa/src/br/com/caelum/financas/modelo/Cliente.java
@@ -0,0 +1,145 @@
+package br.com.caelum.financas.modelo;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
+@SequenceGenerator(name = "SEQ_CONTAS", sequenceName = "SEQ_CONTAS", initialValue = 1)
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "SEQ_CONTAS")
+	private Integer id;
+	@Column
+	private String nome;
+	@Column
+	private String sobrenome;
+	@Column
+	private String matricula;
+	@Column
+	private Boolean ativo;
+	
+	@OneToMany
+	private List<Preferencia> preferencias;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public List<Preferencia> getPreferencias() {
+		return preferencias;
+	}
+
+	public void setPreferencias(List<Preferencia> preferencias) {
+		this.preferencias = preferencias;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((ativo == null) ? 0 : ativo.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((matricula == null) ? 0 : matricula.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((preferencias == null) ? 0 : preferencias.hashCode());
+		result = prime * result + ((sobrenome == null) ? 0 : sobrenome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (ativo == null) {
+			if (other.ativo != null)
+				return false;
+		} else if (!ativo.equals(other.ativo))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (matricula == null) {
+			if (other.matricula != null)
+				return false;
+		} else if (!matricula.equals(other.matricula))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (preferencias == null) {
+			if (other.preferencias != null)
+				return false;
+		} else if (!preferencias.equals(other.preferencias))
+			return false;
+		if (sobrenome == null) {
+			if (other.sobrenome != null)
+				return false;
+		} else if (!sobrenome.equals(other.sobrenome))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", nome=" + nome + ", sobrenome=" + sobrenome + ", matricula=" + matricula
+				+ ", ativo=" + ativo + ", preferencias=" + preferencias + "]";
+	}
+
+
+
+}
---
+++ contas-jpa/src/br/com/caelum/financas/util/PopulaCliente.java
@@ -0,0 +1,162 @@
+package br.com.caelum.financas.util;
+
+import java.util.List;
+import java.util.Scanner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import br.com.caelum.financas.modelo.Cliente;
+import br.com.caelum.financas.modelo.Preferencia;
+
+public class PopulaCliente {
+	EntityManager em = new JPAUtil().getEntityManger();
+
+	public static void popula() {
+		Preferencia preferencia = new Preferencia();
+		Scanner scan = new Scanner(System.in);
+		Cliente cliente = new Cliente();
+
+		System.out.println("digite o nome: ");
+		cliente.setNome(scan.next());
+
+		System.out.println("digite o sobrenome: ");
+		cliente.setSobrenome(scan.next());
+
+		System.out.println("digite atricula: ");
+		cliente.setMatricula(scan.next());
+
+		cliente.setAtivo(true);
+
+		EntityManager em = new JPAUtil().getEntityManger();
+		em.getTransaction().begin();
+		em.persist(cliente);
+		em.getTransaction().commit();
+		em.close();
+
+		System.out.println("Digite 0 para ADD preferencia Ou 1 para não");
+		if (scan.nextInt() == 0) {
+			preferencia.setIdCliente(cliente);
+			preferencia.setPreferencia("CLIENTE PREFERENCIAL");
+
+		} else {
+			preferencia.setIdCliente(cliente);
+			preferencia.setPreferencia("NENHUMA PREFENCIA");
+
+		}
+
+		EntityManager em1 = new JPAUtil().getEntityManger();
+		em1.getTransaction().begin();
+		em1.persist(preferencia);
+		em1.getTransaction().commit();
+		em1.close();
+
+		scan.close();
+	}
+
+	public static void atualiza() {
+		Cliente cliente = new Cliente();
+
+		Scanner scan = new Scanner(System.in);
+		EntityManager em = new JPAUtil().getEntityManger();
+		em.getTransaction().begin();
+		System.out.println("Matricula para atualizar: ");
+		String matricula = scan.next();
+		Query busca = em.createNativeQuery("Select * from cliente where matricula =" + matricula, Cliente.class);
+		cliente = (Cliente) busca.getSingleResult();
+
+		System.out.println("digite o nome: ");
+		cliente.setNome(scan.next());
+
+		System.out.println("digite o sobrenome: ");
+		cliente.setSobrenome(scan.next());
+
+		System.out.println("digite matricula: ");
+		cliente.setMatricula(scan.next());
+		em.merge(cliente);
+		em.getTransaction().commit();
+		em.close();
+
+	}
+
+	public static void remover() {
+		Cliente cliente = new Cliente();
+		Scanner scan = new Scanner(System.in);
+		EntityManager em = new JPAUtil().getEntityManger();
+		em.getTransaction().begin();
+		System.out.println("Matricula para DELETAR: ");
+		String matricula = scan.next();
+		Query remover = em.createNativeQuery("Select * from cliente where matricula = " + matricula, Cliente.class);
+		cliente = (Cliente) remover.getSingleResult();
+		em.remove(cliente);
+		em.getTransaction().commit();
+		em.close();
+
+	}
+
+	public static List<Cliente> listar() {
+		EntityManager em = new JPAUtil().getEntityManger();
+		em.getTransaction().begin();
+		Query consulta = em.createNativeQuery("Select * from cliente", Cliente.class);
+		List<Cliente> lista = consulta.getResultList();
+		for (Cliente c : lista) {
+			System.out.println(c);
+		}
+		em.getTransaction().commit();
+		em.close();
+		return lista;
+
+	}
+
+	public static void ativar() {
+		Cliente cliente = new Cliente();
+
+		Scanner scan = new Scanner(System.in);
+		EntityManager em = new JPAUtil().getEntityManger();
+		em.getTransaction().begin();
+		System.out.println("Matricula para ATIVAR CLIENTE: ");
+		String matricula = scan.next();
+		Query busca = em.createNativeQuery("Select * from cliente where matricula =" + matricula, Cliente.class);
+		cliente = (Cliente) busca.getSingleResult();
+		cliente.setAtivo(true);
+		em.merge(cliente);
+		System.out.println("CLIENTE ATIVADO COM SUSCESSSO");
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	public static void desativar() {
+		Cliente cliente = new Cliente();
+		Scanner scan = new Scanner(System.in);
+		EntityManager em = new JPAUtil().getEntityManger();
+		em.getTransaction().begin();
+		System.out.println("Matricula para DESATIVAR CLIENTE: ");
+		String matricula = scan.next();
+		Query busca = em.createNativeQuery("Select * from cliente where matricula =" + matricula, Cliente.class);
+		cliente = (Cliente) busca.getSingleResult();
+		cliente.setAtivo(false);
+		em.merge(cliente);
+		System.out.println("CLIENTE DESATIVADO COM SUSCESSSO");
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	public static void listarPreferencia() {
+		Cliente cliente = new Cliente();
+		Scanner scan = new Scanner(System.in);
+		EntityManager em = new JPAUtil().getEntityManger();
+		em.getTransaction().begin();
+		System.out.println("Matricula para atualizar: ");
+		String matricula = scan.next();
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("Select * from preferencia");
+		stringBuilder.append("INNER JOIN CLIENTE c ON p.IdCLIENTE_Id = c.ID ");
+        stringBuilder.append("WHERE c.MATRICULA = :matricula");
+        Query query = em.createNativeQuery(stringBuilder.toString(), Preferencia.class);
+        query.setParameter("matricula", matricula);
+        em.getTransaction().commit();
+		em.close();
+
+	}
+
+}
